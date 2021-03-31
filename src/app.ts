0b3a88bfe0bf74ff77d6fdba892fdea3777f0959
Alvaro <alvaro.sebastian98@gmail.com>
2021-03-30 19:02:50 -0500

    extension config

--- src/app.ts
+++ src/app.ts
@@ -16,6 +16,7 @@ import jwt from 'express-jwt';
 import cors from 'cors';
 
 const app: express.Application = express();
+const extension = process.env.NODE_ENV === 'production' ? 'js' : 'ts';
 
 // CORS
 app.use(cors());
@@ -36,8 +37,8 @@ if (process.env.JWT_SECRET_KEY) {
 }
 
 // Controllers: define controllers path
-app.use(loadControllers(`controllers/*.${process.env.NODE_ENV === 'production' ?
-                        'js' : 'ts'}`, {
+app.use(loadControllers(
+    `controllers/*.${extension}`, {
     cwd: __dirname
 }));
 
